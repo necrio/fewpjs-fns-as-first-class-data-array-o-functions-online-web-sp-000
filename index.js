cc56ff84950b256910f00d8c58c17ce9156fb583
Keith Isham <kisham8899@gmail.com>
2020-07-08 20:54:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,34 +1,33 @@
-ar wakeDog = function(dogName="Byron", dogBreed="poodle") {
-  console.log(`Wake ${dogName} the ${dogBreed}`);
-  return `Wake ${dogName} the ${dogBreed}`
+function wakeDog(dogName, dogBreed) {
+    return (`Wake ${dogName} the ${dogBreed}`);
 }
 
-var leashDog = function(dogName="Byron", dogBreed="poodle") {
-  console.log(`Leash ${dogName} the ${dogBreed}`);
-  return `Leash ${dogName} the ${dogBreed}`
+function leashDog(dogName, dogBreed) {
+  return (`Leash ${dogName} the ${dogBreed}`);
 }
 
-var walkToPark = function(dogName="Byron", dogBreed="poodle") {
-  console.log(`Walk to the park with ${dogName} the ${dogBreed}`);
-  return `Walk to the park with ${dogName} the ${dogBreed}`
+function walkToPark(dogName, dogBreed) {
+  return (`Walk to the park with ${dogName} the ${dogBreed}`);
 }
 
-var throwFrisbee = function(dogName="Byron", dogBreed="poodle") {
-  console.log(`Throw the frisbee for ${dogName} the ${dogBreed}`);
-  return `Throw the frisbee for ${dogName} the ${dogBreed}`
+function throwFrisbee(dogName, dogBreed) {
+  return (`Throw the frisbee for ${dogName} the ${dogBreed}`);
 }
 
-var walkHome = function(dogName="Byron", dogBreed="poodle") {
-  console.log(`Walk home with ${dogName} the ${dogBreed}`);
-  return `Walk home with ${dogName} the ${dogBreed}`
+function walkHome(dogName, dogBreed) {
+  return (`Walk home with ${dogName} the ${dogBreed}`);
 }
 
-var unleashDog = function(dogName="Byron", dogBreed="poodle") {
-  console.log(`Unleash ${dogName} the ${dogBreed}`);
-  return `Unleash ${dogName} the ${dogBreed}`
+function unleashDog(dogName, dogBreed) {
+  return (`Unleash ${dogName} the ${dogBreed}`);
 }
 
-const routine = [wakeDog,leashDog,walkToPark,throwFrisbee,walkHome,unleashDog]
+var routine = [wakeDog, leashDog, walkToPark, throwFrisbee, walkHome, unleashDog];
+
+// function exerciseDog(dogName, dogBreed) {
+//   for (var i = 0; i < routine.length; i++) {
+//
+//   }
 
 function exerciseDog(dog, breed) {
   return routine.map(fn => fn(dog, breed))
